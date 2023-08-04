4ef25160a9143d7a0b893109ed0fd09abba8253c
Natalie <natalie.ribero1@gmail.com>
2023-08-04 12:29:21 -0300

    Cree controllers y clase account

---
+++ src/main/java/com/mindhub/homebanking/models/Account.java
@@ -0,0 +1,16 @@
+package com.mindhub.homebanking.models;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    
+}
