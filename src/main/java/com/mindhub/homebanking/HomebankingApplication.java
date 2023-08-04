a9d28024f8b737e6c843693ac9a14a54941edaeb
Natalie <natalie.ribero1@gmail.com>
2023-08-03 22:48:30 -0300

    Guarde la prueba en repository

--- src/main/java/com/mindhub/homebanking/HomebankingApplication.java
+++ src/main/java/com/mindhub/homebanking/HomebankingApplication.java
@@ -1,6 +1,7 @@
 package com.mindhub.homebanking;
 
 import com.mindhub.homebanking.models.Client;
+import com.mindhub.homebanking.repositories.ClientRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,9 +14,11 @@ public class HomebankingApplication {
 		SpringApplication.run(HomebankingApplication.class, args);
 	}
 	@Bean
-	public CommandLineRunner initData(){
+	public CommandLineRunner initData(ClientRepository clientRepository){
 		return(args) -> {
 			Client client1 = new Client("Melba", "Morel","melba@mindhub.com");
+
+			clientRepository.save(client1);
 		};
 
 	}
