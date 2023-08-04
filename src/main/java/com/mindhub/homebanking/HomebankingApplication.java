87c07dfaaf91f1784a80fdce30ee98af6e3eca1b
Natalie <natalie.ribero1@gmail.com>
2023-08-03 22:08:29 -0300

    Cree datos de prueba

--- src/main/java/com/mindhub/homebanking/HomebankingApplication.java
+++ src/main/java/com/mindhub/homebanking/HomebankingApplication.java
@@ -1,5 +1,6 @@
 package com.mindhub.homebanking;
 
+import com.mindhub.homebanking.models.Client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class HomebankingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomebankingApplication.class, args);
+		Client client1 = new Client("Melba", "Morel", "Melba@mindhub.com");
 	}
 
 }
