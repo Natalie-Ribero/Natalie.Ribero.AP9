a8f7dc566af11d78e1fd82443909917241eec1d9
Natalie <natalie.ribero1@gmail.com>
2023-08-03 22:42:46 -0300

    Puse el beans para agregar clientes

--- src/main/java/com/mindhub/homebanking/HomebankingApplication.java
+++ src/main/java/com/mindhub/homebanking/HomebankingApplication.java
@@ -1,15 +1,22 @@
 package com.mindhub.homebanking;
 
 import com.mindhub.homebanking.models.Client;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HomebankingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomebankingApplication.class, args);
-		Client client1 = new Client("Melba", "Morel", "Melba@mindhub.com");
 	}
+	@Bean
+	public CommandLineRunner initData(){
+		return(args) -> {
+			Client client1 = new Client("Melba", "Morel","melba@mindhub.com");
+		};
 
+	}
 }
