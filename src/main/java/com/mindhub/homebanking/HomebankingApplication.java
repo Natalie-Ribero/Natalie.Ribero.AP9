31ab01d5fc610473f472e064e677b5ef37d2a64a
Natalie <natalie.ribero1@gmail.com>
2023-08-07 19:58:25 -0300

    agregue archivos a static

--- src/main/java/com/mindhub/homebanking/HomebankingApplication.java
+++ src/main/java/com/mindhub/homebanking/HomebankingApplication.java
@@ -1,11 +1,14 @@
 package com.mindhub.homebanking;
 
+import com.mindhub.homebanking.models.Account;
 import com.mindhub.homebanking.models.Client;
 import com.mindhub.homebanking.repositories.ClientRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class HomebankingApplication {
@@ -22,6 +25,10 @@ public class HomebankingApplication {
 			clientRepository.save(client2);
 			Client client3 = new Client("Daniel", "Estebez","daniestebez@mindhub.com");
 			clientRepository.save(client3);
+
+			LocalDate Date = LocalDate.now();
+
+			Account acount1 = new Account("VIN001",Date,5000);
 		};
 	}
 }
