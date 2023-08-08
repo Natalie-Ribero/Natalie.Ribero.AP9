fb5e6bd1064ffd2a4a14f30884a3df9e75d0b82a
Natalie <natalie.ribero1@gmail.com>
2023-08-08 10:31:56 -0300

    Agregue el controller de client

---
+++ src/main/java/com/mindhub/homebanking/controllers/ClientController.java
@@ -0,0 +1,28 @@
+package com.mindhub.homebanking.controllers;
+
+import com.mindhub.homebanking.models.Client;
+import com.mindhub.homebanking.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class ClientController {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @GetMapping("/clients")
+        public List<Client> getClients(){
+        return clientRepository.findAll();
+    }
+/*
+    @GetMapping("/clients/id{id}")
+    public void getClientsById(){
+        return clientRepository.findById(Long);
+    } */
+}
