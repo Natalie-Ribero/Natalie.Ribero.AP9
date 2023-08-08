2cd96111aa70402960bc5138c1f6650f9798e495
Natalie <natalie.ribero1@gmail.com>
2023-08-08 12:28:13 -0300

    Cambie los setters de clientdto a getters

--- src/main/java/com/mindhub/homebanking/dtos/ClientDTO.java
+++ src/main/java/com/mindhub/homebanking/dtos/ClientDTO.java
@@ -15,19 +15,19 @@ public class ClientDTO {
         this.email = client.getEmail();
     }
 
-    public void setId(long id) {
-        this.id = id;
+    public long getId() {
+        return id;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public String getLastName() {
+        return lastName;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public String getEmail() {
+        return email;
     }
 }
