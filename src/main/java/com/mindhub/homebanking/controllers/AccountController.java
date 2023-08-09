82723a0ddec0e6668a37cb93889f5be29bc30931
Natalie <natalie.ribero1@gmail.com>
2023-08-08 22:28:46 -0300

    Cree ruta con id y solo de accounts

--- src/main/java/com/mindhub/homebanking/controllers/AccountController.java
+++ src/main/java/com/mindhub/homebanking/controllers/AccountController.java
@@ -1,9 +1,19 @@
 package com.mindhub.homebanking.controllers;
 
+import com.mindhub.homebanking.dtos.AccountDTO;
+import com.mindhub.homebanking.dtos.ClientDTO;
+import com.mindhub.homebanking.models.Account;
+import com.mindhub.homebanking.models.Client;
 import com.mindhub.homebanking.repositories.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
@@ -11,5 +21,19 @@ public class AccountController {
     @Autowired
     private AccountRepository accountRepository;
 
-    @get
+    @GetMapping("/accounts")
+    public List<AccountDTO> getAccount() {
+        List<Account> allAccounts = accountRepository.findAll();
+        List<AccountDTO> allAccountsDTO = allAccounts
+                .stream()
+                .map(account -> new AccountDTO(account))
+                .collect(Collectors.toList());
+        return allAccountsDTO;
+    }
+
+    @GetMapping("/accounts/{id}")
+    public AccountDTO getAccountsById(@PathVariable Long id){
+        Optional<Account> account = accountRepository.findById(id);
+        return new AccountDTO(account.get());
+    }
 }
