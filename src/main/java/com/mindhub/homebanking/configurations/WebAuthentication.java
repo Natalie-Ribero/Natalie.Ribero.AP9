5e510c71635e3fec59176c6bc4a2ce5d8e0773bc
natalie-enova <natalie.ribero1@gmail.com>
2023-08-18 11:03:09 -0300

    Agregue un cliente con el rol de admin

--- src/main/java/com/mindhub/homebanking/configurations/WebAuthentication.java
+++ src/main/java/com/mindhub/homebanking/configurations/WebAuthentication.java
@@ -20,34 +20,22 @@ public class WebAuthentication extends GlobalAuthenticationConfigurerAdapter {
     private ClientRepository clientRepository;
 
     @Override
-
     public void init(AuthenticationManagerBuilder auth) throws Exception {
-
         auth.userDetailsService(inputName -> {
-
             Client client = clientRepository.findByEmail(inputName);
 
             if (client != null) {
-
                 return new User(client.getEmail(), client.getPassword(),
-
                         AuthorityUtils.createAuthorityList("CLIENT"));
-
             } else {
-
                 throw new UsernameNotFoundException("Unknown user: " + inputName);
-
             }
-
         });
     }
 
     @Bean
-
     public PasswordEncoder passwordEncoder() {
-
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-
     }
 
 }
