8035338e0b15de2080e6e3d9b810caea6c6c599e
natalie-enova <natalie.ribero1@gmail.com>
2023-08-17 21:12:32 -0300

    Agregue la carpeta configurations y puse la clase para modificar el servicio de detalles de usuario

---
+++ src/main/java/com/mindhub/homebanking/configurations/WebAuthentication.java
@@ -0,0 +1,6 @@
+package com.mindhub.homebanking.configurations;
+
+import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
+
+public class WebAuthentication extends GlobalAuthenticationConfigurerAdapter {
+}
