57a209dfa0474dca85b1f9397957ad89f030a42e
natalie-enova <natalie.ribero1@gmail.com>
2023-08-17 21:52:40 -0300

    cambie detalles

--- src/main/java/com/mindhub/homebanking/configurations/WebAuthentication.java
+++ src/main/java/com/mindhub/homebanking/configurations/WebAuthentication.java
@@ -1,6 +1,11 @@
 package com.mindhub.homebanking.configurations;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
 
+@Configuration
 public class WebAuthentication extends GlobalAuthenticationConfigurerAdapter {
+
+    
+
 }
