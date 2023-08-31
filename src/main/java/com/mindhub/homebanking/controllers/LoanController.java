801e72fa12363bad0852c57781d4a968e8ddb396
natalie-enova <natalie.ribero1@gmail.com>
2023-08-31 11:01:46 -0300

    Agregue carpeta static de task9

--- src/main/java/com/mindhub/homebanking/controllers/LoanController.java
+++ src/main/java/com/mindhub/homebanking/controllers/LoanController.java
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class LoanController {
     @Transactional
-    @RequestMapping(path = "/transactions", method = RequestMethod.POST)
-    public ResponseEntity<Object> transaction(){
+    @RequestMapping(path = "/loans", method = RequestMethod.POST)
+    public ResponseEntity<Object> applyLoans(){
 
     }
 }
