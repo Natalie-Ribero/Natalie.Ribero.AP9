57668688bcd2c2d7844b787fc3ac6347b9c1b140
natalie-enova <natalie.ribero1@gmail.com>
2023-08-31 11:06:53 -0300

    Agregue loanDTO

--- src/main/java/com/mindhub/homebanking/controllers/LoanController.java
+++ src/main/java/com/mindhub/homebanking/controllers/LoanController.java
@@ -1,17 +1,31 @@
 package com.mindhub.homebanking.controllers;
 
+import com.mindhub.homebanking.dtos.ClientDTO;
+import com.mindhub.homebanking.repositories.LoanRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
 public class LoanController {
+    @Autowired
+    LoanRepository loanRepository;
+
+    @GetMapping("/loans")
+    public List<LoanDTO> getLoans() {
+        return loanRepository.findAll()
+                .stream()
+                .map(LoanDTO::new)
+                .collect(Collectors.toList());
+    }
     @Transactional
     @RequestMapping(path = "/loans", method = RequestMethod.POST)
-    public ResponseEntity<Object> applyLoans(){
+    public ResponseEntity<Object> applyLoans(@RequestBody ){
 
     }
 }
