7d8fc7a5f3e4a0f03f9cf863ed3ff45830afe371
natalie-enova <natalie.ribero1@gmail.com>
2023-08-31 10:57:11 -0300

    Cree DTO de aplication

--- src/main/java/com/mindhub/homebanking/controllers/LoanController.java
+++ src/main/java/com/mindhub/homebanking/controllers/LoanController.java
@@ -1,4 +1,17 @@
 package com.mindhub.homebanking.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class LoanController {
+    @Transactional
+    @RequestMapping(path = "/transactions", method = RequestMethod.POST)
+    public ResponseEntity<Object> transaction(){
+
+    }
 }
